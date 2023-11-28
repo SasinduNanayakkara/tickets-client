aaeee76a839f02f526b2daf271303c9d213ea315
SasinduNanayakkara <it20625016@my.sliit.lk>
2023-11-28 22:10:46 +0530

    Implement Notification bar

---
+++ src/Components/Notification.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Notification() {
+  return (
+    <div className='h-12 bg-[#E50914] text-white px-4 flex items-center justify-center text-center text-sm font-semibold md:text-base cursor-pointer'>Special Order</div>
+  )
+}
+
+export default Notification

98952cbcc41d0173c80f00d10317f1b46ff20c11
SasinduNanayakkara <it20625016@my.sliit.lk>
2023-12-04 02:34:02 +0530

    Implement login & register UI and fixes

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-white flex flex-col items-center justify-between'>
+    <div className='p-4 lg:p-4 xl:p-4 text-white flex flex-col items-center justify-between border-y-2 border-y-[#E50914] mt-4 xl:mt-10'>
       <Link href={"/"} className='font-bold text-xl'>GRAB YOUR TICKETS</Link>
       <p>ALL RIGHTS RESERVED © 2023</p>
     </div>

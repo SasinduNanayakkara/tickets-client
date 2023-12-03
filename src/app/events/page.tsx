a40ff09cddb83de20d22d10b4c6617ac77614639
SasinduNanayakkara <it20625016@my.sliit.lk>
2023-12-04 02:34:58 +0530

    GYT-008 Implement Single event

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,8 +1,15 @@
+import Categories from '@/Components/Categories'
+import Event from '@/Components/Event'
+import Slider from '@/Components/Slider'
 import React from 'react'
 
 function EventsPage() {
   return (
-    <div>EventsPage</div>
+    <main>
+      <Slider/>
+      <Categories/>
+      <Event/>
+    </main>
   )
 }
 

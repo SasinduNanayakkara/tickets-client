e43e914b7c6342173890feaeab196bdb2391d35e
SasinduNanayakkara <it20625016@my.sliit.lk>
2024-01-16 03:10:24 +0530

    Implement Digital ticket and features

--- src/app/api/Ticket.ts
+++ src/app/api/Ticket.ts
@@ -12,3 +12,34 @@ export const getTicketData = async (id:string, accessToken:string) => {
         console.log(err);
     }
 }
+
+export const ticketDownload = async (eventImage: string, paymentRef: string, location: string, eventName: string, quantity: number, date: string, time: string, ticketPrice: number, ticketId: string, amount: number, accessToken: string) => {
+    try {
+        console.log("ok");
+        const result = await axios.post(`${baseUrl}/ticket/download`, 
+        {eventImage, paymentRef, location, eventName, quantity, date, time, ticketPrice, ticketId, amount}, 
+        {headers: {
+            Authorization: `Bearer ${accessToken}`
+        }, responseType: 'blob'});
+        console.log("pdf data ", result);
+        
+        const url = new Blob([result.data], { type: 'application/pdf' });
+        console.log("URL - ", url);
+        
+        const link = document.createElement('a');
+        link.href = window.URL.createObjectURL(url);
+        link.download = 'Digital-Ticket.pdf';
+
+      // Append the link to the document
+      document.body.appendChild(link);
+
+      // Trigger the click event on the link to start the download
+      link.click();
+
+      // Remove the link from the document
+      document.body.removeChild(link);
+    }
+    catch(err) {
+        console.log(err);
+    }
+}
